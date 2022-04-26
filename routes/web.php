ab8bf6d2812b4857d9e5511eadf13fb673d6ab69
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-04-26 15:45:30 +0200

    passato il dato name

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $parametri = [
+        'name' => 'Giuseppe'
+    ];
+    return view('home', $parametri);
 });
